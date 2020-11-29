38df8b93c39b853d65316549cfa258ca07b1b3fa
Aikepaer Abuduweili <abdvls@gmail.com>
2020-11-29 14:58:04 -0800

    Create Proxify.ts

---
+++ Proxify.ts
@@ -0,0 +1,22 @@
+type Proxy<T> = {
+    get(): T;
+    set(value: T): void;
+}
+type Proxify<T> = {
+    [P in keyof T]: Proxy<T[P]>;
+}
+function proxify<T>(o: T): Proxify<T> {
+   // ... wrap proxies ...
+}
+let proxyProps = proxify(props);
+
+// unproxify
+function unproxify<T>(t: Proxify<T>): T {
+    let result = {} as T;
+    for (const k in t) {
+        result[k] = t[k].get();
+    }
+    return result;
+}
+
+let originalProps = unproxify(proxyProps);
