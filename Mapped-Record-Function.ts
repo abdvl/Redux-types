0aa29f9c57d220f7972171313622238bd5731278
Aikepaer Abuduweili <abdvls@gmail.com>
2020-11-29 15:03:37 -0800

    Update Mapped-Record-Function.ts

--- Mapped-Record-Function.ts
+++ Mapped-Record-Function.ts
@@ -3,13 +3,28 @@ const t = {
     k2: null,
 }
 
+// simple 
+
+type A = Record<keyof typeof t, (...args : any[]) => any>;
+const a: A = {
+    k1: () => null,
+    k2: (arg1: number) => null,
+    k3: 1, // err now
+}
+
+a.k1()
+a.k2()  // err now, but no parameter tips
+
+
+// complex
+// !! Record<Exclude<keyof U, keyof T>, never>
 function wrapperFN<T>() {
     return <U extends Record<keyof T, (...args: any[]) => void>>(o: U & Record<Exclude<keyof U, keyof T>, never>) => o
 }
-const a = wrapperFN<typeof t>()({
+const b = wrapperFN<typeof t>()({
     k1: () => null,
     k2: (arg1: number) => null,
     k3: 1, // err now
 });
-a.k1()
-a.k2() // err now
+b.k1()
+b.k2() // err now, with parameter tips
